303e0d3d2528ee4ecb94eceda8fe4e415c17c62a
Laurine-vtg <152720415+Laurine-vtg@users.noreply.github.com>
2023-12-13 17:43:21 +0100

    Update traceur_profil_F-V.py

--- traceur_profil_F-V.py
+++ traceur_profil_F-V.py
@@ -75,9 +75,9 @@ with st.sidebar:
     coarse_start_index = st.number_input('Numéro de ligne de début', min_value=0, max_value=len(data) - 1, value=0)
     coarse_end_index = st.number_input('Numéro de ligne de fin', min_value=coarse_start_index, max_value=len(data) - 1, value=len(data) - 1)
     
-with st.sidebar.container():
-    image = Image.open(r'C:\Users\lauri\OneDrive\Documents\M2\STAGE\PROFIL_FV\photo1.png')
-    st.image(image, use_column_width=True, caption = 'Exemple de sélection approxixative du sprint')
+#with st.sidebar.container():
+   # image = Image.open(r'C:\Users\lauri\OneDrive\Documents\M2\STAGE\PROFIL_FV\photo1.png')
+    #st.image(image, use_column_width=True, caption = 'Exemple de sélection approxixative du sprint')
 
 # Afficher les données sélectionnées grossièrement
 #st.write("Sélection grossière:")
@@ -89,9 +89,9 @@ with st.sidebar:
     fine_range_selector = st.slider('Numéros de ligne', min_value=coarse_start_index, max_value=coarse_end_index,
                                     value=(coarse_start_index, coarse_end_index))
     
-with st.sidebar.container():
-    image1 = Image.open(r'C:\Users\lauri\OneDrive\Documents\M2\STAGE\PROFIL_FV\photo2.png')
-    st.image(image1, use_column_width=True, caption = 'Exemple de sélection précise du sprint')
+#with st.sidebar.container():
+    #image1 = Image.open(r'C:\Users\lauri\OneDrive\Documents\M2\STAGE\PROFIL_FV\photo2.png')
+    #st.image(image1, use_column_width=True, caption = 'Exemple de sélection précise du sprint')
 
 st.sidebar.write('<span style="color: green;">Une fois que votre sprint ressemble à celui en exemple juste au dessus, vous pouvez valider votre sprint.</span>', unsafe_allow_html=True)
 
